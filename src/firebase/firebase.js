43a0f29d51225cae6466465912dc4bed938a84a3
Ujjwal Gupta <125856546+ujjwal7007@users.noreply.github.com>
2023-06-20 00:45:21 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDl1o5D3l2hbpHbPUQuOYgz7y9rvaAXRw",
+  apiKey:"",
   authDomain: "filmyverse-4d1c0.firebaseapp.com",
   projectId: "filmyverse-4d1c0",
   storageBucket: "filmyverse-4d1c0.appspot.com",
